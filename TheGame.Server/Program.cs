6e251768ee0a4a01a07079bc0ba612b56d30b468
Nikolas <nikolas.hugsted@bekk.no>
2022-03-06 13:51:13 +0100

    Add game loop to server

--- TheGame.Server/Program.cs
+++ TheGame.Server/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Logging;
+
+using TheGame.Core;
+
+var gameLoopOptions = new GameLoopOptions(tickRate: 1);
+
+var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddConsole();
+});
+
+var logger = loggerFactory.CreateLogger<GameLoop>();
+
+var gameLoop = new GameLoop(gameLoopOptions, logger);
+await gameLoop.Run((TimeSpan delta) => { });
