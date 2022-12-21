4a591d5e79eb4f23c72e43cb2fd036a0d97b42e1
Nikolas <13964858+nikolasrh@users.noreply.github.com>
2022-12-21 20:40:47 +0100

    Start GodotClient aswell as ConsoleClient

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 dotnet build
 
 mintty bash -c "dotnet run --project TheGame.GameServer; read -p \"Press enter to close the window...\""
+
 sleep 3
-mintty bash -c "dotnet run --project TheGame.GameClient; read -p \"Press enter to close the window...\""
-mintty bash -c "dotnet run --project TheGame.GameClient; read -p \"Press enter to close the window...\""
+
+mintty bash -c "dotnet run --project TheGame.ConsoleClient; read -p \"Press enter to close the window...\""
+
+mintty bash -i -c "cd TheGame.GodotClient; godot; read -p \"Press enter to close the window...\""
