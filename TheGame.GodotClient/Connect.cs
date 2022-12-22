2e14edc2a578bf4c87788f5c98261f1b964d8a89
Nikolas <13964858+nikolasrh@users.noreply.github.com>
2022-12-22 22:10:20 +0100

    Add error message to connection screen

--- TheGame.GodotClient/Connect.cs
+++ TheGame.GodotClient/Connect.cs
@@ -5,16 +5,24 @@ public partial class Connect : VBoxContainer
     public override void _Ready()
     {
         var game = GetNode<GameNode>("/root/GameNode").Game;
-        var hostname = GetNode<TextInput>("Hostname");
-        var port = GetNode<NumberInput>("Port");
-        var playerName = GetNode<TextInput>("PlayerName");
-        var submitButton = GetNode<Button>("MarginContainer/Button");
+        var hostname = GetNode<TextInput>("VBoxContainer/HostnameInput");
+        var port = GetNode<NumberInput>("VBoxContainer/PortInput");
+        var playerName = GetNode<TextInput>("VBoxContainer/PlayerNameInput");
+        var connectButton = GetNode<Button>("ConnectButton");
+        var errorMessage = GetNode<Label>("ErrorMessage");
+        errorMessage.Visible = false;
 
-        submitButton.Pressed += () =>
+        connectButton.Pressed += () =>
         {
-            game.Connect(hostname.Value, port.Value);
-            game.JoinGame(playerName.Value);
-            GetTree().ChangeSceneToFile("res://Main.tscn");
+            if (game.Connect(hostname.Value, port.Value))
+            {
+                game.JoinGame(playerName.Value);
+                GetTree().ChangeSceneToFile("res://Main.tscn");
+            }
+            else
+            {
+                errorMessage.Visible = true;
+            }
         };
     }
 }
